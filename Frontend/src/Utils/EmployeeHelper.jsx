5cb1e6f3fc7c13a0f5edf8ac009195d0a3b0ba75
Godson Nudzor <godsonnudzor@gmail.com>
2025-10-21 14:52:21 +0000

    added new delete button

--- Frontend/src/Utils/EmployeeHelper.jsx
+++ Frontend/src/Utils/EmployeeHelper.jsx
@@ -61,6 +61,9 @@ export const fetchDepartments = async () => {
             <button className="px-3 py-1 bg-blue-400 text-white" >
                 Edit
             </button>
+            <button className="px-3 py-1 bg-red-600 text-white" >
+                Delete
+            </button>
             <button className="px-3 py-1 bg-yellow-600 text-white" >
                 Salary
             </button>
