911df8090f9dc2b79d1673c93dc931fc6d052a32
Godson Nudzor <godsonnudzor@gmail.com>
2025-12-01 11:05:19 +0000

    eslint-disable-next-line react-refresh/only-export-components

--- Frontend/src/Utils/DepartmentHelper.jsx
+++ Frontend/src/Utils/DepartmentHelper.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios' 
+
+// eslint-disable-next-line react-refresh/only-export-components
 export const columns = [
     {
         name : "SNO",
